1e498d8aecb0d64b95954cdab64b852a2f9374de
Joaquim50 <joaquim.metaphi@gmail.com>
2025-12-11 10:36:57 +0530

    CategoryList API integration

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import portfolioReducer from './slices/portfolioSlice';
+import categoryReducer from './slices/categorySlice';
 
 export const store = configureStore({
     reducer: {
         portfolio: portfolioReducer,
+        category: categoryReducer,
     },
 });
